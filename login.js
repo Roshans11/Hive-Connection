0974aa0cc1e61e2c4fe7038351be8eb8838dd178
Roshans11 <singhroshanb1@gmail.com>
2025-09-09 00:02:12 +0530

    2nd commit

--- login.js
+++ login.js
@@ -1,13 +1,14 @@
 // === Firebase Config ===
-const firebaseConfig = {
-  apiKey: "...",
-  authDomain: "...",
-  projectId: "...",
-  storageBucket: "...",
-  messagingSenderId: "...",
-  appId: "...",
-  measurementId: "..."
-};
+ const firebaseConfig = {
+    apiKey: "AIzaSyAWSnfnPvFLFnUh4KjM94jbYnj2aKY8FNc",
+    authDomain: "hive-connection-3294d.firebaseapp.com",
+    databaseURL: "https://hive-connection-3294d-default-rtdb.firebaseio.com",
+    projectId: "hive-connection-3294d",
+    storageBucket: "hive-connection-3294d.firebasestorage.app",
+    messagingSenderId: "1050669847051",
+    appId: "1:1050669847051:web:d2c502c30ada68e90aed0e",
+    measurementId: "G-BNHH4DQWMS"
+  };
 firebase.initializeApp(firebaseConfig);
 
 const auth = firebase.auth();
