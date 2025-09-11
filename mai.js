7771cb606aa34b3b6ad0c84dd45a4fe8670c9ee0
Roshans11 <singhroshanb1@gmail.com>
2025-09-11 22:39:05 +0530

    commit

---
+++ mai.js
@@ -0,0 +1,11 @@
+ const firebaseConfig = {
+    apiKey: "AIzaSyAWSnfnPvFLFnUh4KjM94jbYnj2aKY8FNc",
+    authDomain: "hive-connection-3294d.firebaseapp.com",
+    databaseURL: "https://hive-connection-3294d-default-rtdb.firebaseio.com",
+    projectId: "hive-connection-3294d",
+    storageBucket: "hive-connection-3294d.firebasestorage.app",
+    messagingSenderId: "1050669847051",
+    appId: "1:1050669847051:web:d2c502c30ada68e90aed0e",
+    measurementId: "G-BNHH4DQWMS"
+  };
+  firebase.initializeApp(firebaseConfig);
